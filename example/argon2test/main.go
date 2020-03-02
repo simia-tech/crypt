23b0025e2ddb953bbd426f84a0360ba57268385e
Philipp Brüll <pb@simia.tech>
2020-03-02 11:07:04 +0100

    fixed example

--- example/argon2test/main.go
+++ example/argon2test/main.go
@@ -13,12 +13,13 @@ func main() {
 		m = flag.Int("m", 65536, "memory cost in kb")
 		t = flag.Int("t", 3, "time cost")
 		p = flag.Int("p", 1, "number of threads")
+		k = flag.Int("k", 32, "key size")
 	)
 	flag.Parse()
 
 	password, salt := flag.Arg(0), flag.Arg(1)
 
-	settings, err := crypt.Argon2iSettings(*m, *t, *p, crypt.Base64Encoding.EncodeToString([]byte(salt)))
+	settings, err := crypt.Argon2iSettings(*m, *t, *p, *k, crypt.Base64Encoding.EncodeToString([]byte(salt)))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
